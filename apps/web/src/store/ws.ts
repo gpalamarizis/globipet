776258bff158c4a6e1af8546bb538775d8870e8c
gpalamarizis <gpalamarizis@gmail.com>
2026-06-22 11:23:19 +0300

    Add missing ws store for real-time WebSocket messages

---
+++ apps/web/src/store/ws.ts
@@ -0,0 +1,11 @@
+﻿import { create } from 'zustand'
+
+interface WsState {
+  lastMessage: any | null
+  setLastMessage: (msg: any) => void
+}
+
+export const useWsStore = create<WsState>((set) => ({
+  lastMessage: null,
+  setLastMessage: (msg) => set({ lastMessage: msg }),
+}))
